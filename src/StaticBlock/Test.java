ec3710773e00768cfedf334a2f1bb665db49f369
IMTRK <52650140+Tanmay13@users.noreply.github.com>
2020-03-07 11:53:37 +0530

    Adding the system.exit  deatails
    Status code

--- src/StaticBlock/Test.java
+++ src/StaticBlock/Test.java
@@ -6,6 +6,7 @@ public class Test {
 	{
 	System.out.println("Welcome to static block");	
 	System.exit(0);
+       System.out.println("Here System.exit(0) indicate the status code that is 0 is ok for status is ok");
 		
 	}
 
